27a1373ce9190a2ffe9222c8d2b2b9d5c3de488e
Enrique Rimando <me@elrmndo.dev>
2023-02-02 14:15:54 +0800

    chore: add route configs

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,4 +3,6 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('app', () => ({
   name: process.env.APP_NAME,
   shortName: process.env.APP_SHORT_NAME,
+  port: parseInt(process.env.PORT, 10),
+  routePrefix: process.env.APP_ROUTE_PREFIX,
 }));
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix(configService.get<string>('app.routePrefix'), {
+    exclude: ['/'],
+  });
+
+  app.enableVersioning({ type: VersioningType.URI });
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  await app.listen(configService.get<number>('app.port'));
 }
 
 void bootstrap();
