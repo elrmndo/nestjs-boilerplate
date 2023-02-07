92acb39f87ea6f1e53987d34f09351be9c53cdf3
Enrique Rimando <elrmndo@gmail.com>
2023-02-07 10:24:45 +0800

    refactor: add zod config validation

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,9 +1,26 @@
 import { registerAs } from '@nestjs/config';
+import { z } from 'zod';
 
-export default registerAs('app', () => ({
-  name: process.env.APP_NAME,
-  shortName: process.env.APP_SHORT_NAME,
-  port: parseInt(process.env.PORT, 10),
-  routePrefix: process.env.APP_ROUTE_PREFIX,
-  isProduction: process.env.NODE_ENV === 'production',
-}));
+const AppConfigSchema = z
+  .object({
+    name: z.string(),
+    shortName: z.string(),
+    port: z.number(),
+    routePrefix: z.string(),
+    isProduction: z.boolean(),
+  })
+  .required();
+
+type AppConfig = z.infer<typeof AppConfigSchema>;
+
+export default registerAs('app', (): AppConfig => {
+  const appConfig: AppConfig = {
+    name: process.env.APP_NAME,
+    shortName: process.env.APP_SHORT_NAME,
+    port: parseInt(process.env.PORT, 10),
+    routePrefix: process.env.APP_ROUTE_PREFIX,
+    isProduction: process.env.NODE_ENV === 'production',
+  };
+
+  return AppConfigSchema.parse(appConfig);
+});
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,12 +1,32 @@
 import { registerAs } from '@nestjs/config';
+import { z } from 'zod';
 
-export default registerAs('database', () => ({
-  type: process.env.DATABASE_TYPE,
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  name: process.env.DATABASE_NAME,
-  sync: process.env.DATABASE_SYNCHRONIZE === 'true',
-  maxConnections: parseInt(process.env.DATABASE_MAX_CONNECTIONS, 10),
-}));
+const DatabaseConfigSchema = z
+  .object({
+    type: z.string(),
+    host: z.string(),
+    port: z.number(),
+    username: z.string(),
+    password: z.string(),
+    name: z.string(),
+    sync: z.boolean(),
+    maxConnections: z.number(),
+  })
+  .required();
+
+type DatabaseConfig = z.infer<typeof DatabaseConfigSchema>;
+
+export default registerAs('database', (): DatabaseConfig => {
+  const databaseConfig: DatabaseConfig = {
+    type: process.env.DATABASE_TYPE,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+    sync: process.env.DATABASE_SYNCHRONIZE === 'true',
+    maxConnections: parseInt(process.env.DATABASE_MAX_CONNECTIONS, 10),
+  };
+
+  return DatabaseConfigSchema.parse(databaseConfig);
+});
